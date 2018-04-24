72fab516a5ce99506ea3d79bf401b668d0dbb73c
DavidLDM <32276522+DavidLDM@users.noreply.github.com>
2018-04-24 22:09:27 +0200

    Add files via upload

---
+++ practica28/ejercicio1/ejercicio1.2  1.3 y 1.4/codigo.js
@@ -0,0 +1,7 @@
+
+            alert("David Martinez Montero")
+            alert("Esto funciona")
+
+//comentario de una linea
+
+/*la etiqueta "alert" muestra por pantalla un mensaje emergente con un botón de "ok". Podemos poner varias alertas que se iran sucediendo una tras otra mientras le demos al botón "ok"*/
